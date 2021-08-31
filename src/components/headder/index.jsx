b16d968bc725fc4d7ed8878cd415fd17d34a63f9
appiness <appiness@appinesss-MacBook-Pro.local>
2021-08-31 17:28:57 +0530

    headding added;

--- src/components/headder/index.jsx
+++ src/components/headder/index.jsx
@@ -16,16 +16,26 @@ const Headder = () => {
     <>
       <Row>
         <Col md={24} xs={24}>
-          <Row className="headder-main-div" align="middle" justify="space-between">
-            <Col md={12} xs={12}><img src={Logo} alt="Logo" loading="lazy" className="headder-logo" /></Col>
+          <Row className="headder-main-div" align="middle">
+            <Col md={8} xs={12}><img src={Logo} alt="Logo" loading="lazy" className="headder-logo" /></Col>
             {users ? (
-              <Col md={12} xs={12} style={{ textAlign: 'right' }}>
-                <Button type="text" onClick={handleLogOut}>
-                  <Row align="middle">
-                    Logout&nbsp;
-                    <LogoutOutlined />
-                  </Row>
-                </Button>
+              <Col md={16} xs={12}>
+                <Row justify="space-around" align="middle">
+                  <Col md={12} xs={12} style={{ textAlign: 'center' }}>
+                    <h6 className="headder-Name">
+                      Welcome&nbsp;
+                      {users?.displayName || users?.email || ''}
+                    </h6>
+                  </Col>
+                  <Col md={12} xs={12} style={{ textAlign: 'right' }}>
+                    <Button type="text" onClick={handleLogOut}>
+                      <Row align="middle">
+                        Logout&nbsp;
+                        <LogoutOutlined />
+                      </Row>
+                    </Button>
+                  </Col>
+                </Row>
               </Col>
             ) : ''}
           </Row>
